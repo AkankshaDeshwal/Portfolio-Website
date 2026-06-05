19c4431fb67483046b644379b6e63732a6ced594
AkankshaDeshwal <adeshwal85@gmail.com>
2026-06-05 11:55:43 +0530

    feat:Add ProjectShowcase placeholder section

---
+++ src/sections/ProjectShowcase.jsx
@@ -0,0 +1,11 @@
+import projectImage from '../assets/images/img1.png'
+
+const ProjectShowcase = () => {
+    return ( 
+        <div className="container">
+            <img src={projectImage}  alt='image'/>
+        </div>
+     );
+}
+ 
+export default ProjectShowcase;
