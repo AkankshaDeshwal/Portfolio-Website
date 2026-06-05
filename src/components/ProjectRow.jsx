d982529be709f932fde7781f6aaf733f55373e04
AkankshaDeshwal <adeshwal85@gmail.com>
2026-06-05 12:27:45 +0530

    style:Add text color

--- src/components/ProjectRow.jsx
+++ src/components/ProjectRow.jsx
@@ -4,7 +4,7 @@ import PageLink from "./PageLink";
 const ProjectRow = ({project}) => {
     return ( 
         <>
-        <div className="grid grid-cols-6 w-full font-secondary text-sm ">
+        <div className="grid grid-cols-6 w-full font-secondary text-sm text-foreground">
             <div className="col-span-1 text-muted italic font-semibold">{project.year}</div>
             <div className="col-span-3"><PageLink name={project.name} link={project.link} /></div>
             <div className="col-span-2 text-right">[{project.techStack.map(tech => `${tech} ,`)}]</div>
