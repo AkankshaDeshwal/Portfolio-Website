e3be9d9f4e32a0b8c9901db3ce94ef4117a8252e
AkankshaDeshwal <adeshwal85@gmail.com>
2026-06-05 17:00:37 +0530

    fix:Implement blog cards in columns in mobile screen and rows in others

--- src/sections/TechBlog.jsx
+++ src/sections/TechBlog.jsx
@@ -4,7 +4,7 @@ const TechBlogs = () => {
     return ( 
         <div className="w-full">
             <h1 className="text-l text-foreground font-semibold font-primary mb-6">TECH BLOGS WRITTEN</h1>
-            <div className="flex gap-4">
+            <div className="flex gap-4 flex-col md:flex-row">
                 <TechBlogCard />
                 <TechBlogCard />
                 <TechBlogCard />
