2c272440fb7bae7cbec8b86ed75a58fea844120f
AkankshaDeshwal <adeshwal85@gmail.com>
2026-06-17 22:58:39 +0530

    feat:Implement footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Hero from "./sections/Hero"
 import ProjectLog from "./sections/ProjectLog"
 import ProjectShowcase from "./sections/ProjectShowcase"
 import TechBlogs from "./sections/TechBlog"
+import footerImage from './assets/images/footerImage.avif'
+import Footer from "./sections/Footer"
 
 
 function App() {
@@ -21,12 +23,13 @@ function App() {
     
     <main className="w-full bg-background">
       <Header selectedTheme='Light' toggleThemeHandler={toggleTheme} />
-      <div className="flex flex-col items-start gap-10 mt-8 mx-auto p-8 md:max-w-1/2 md:mt-30 ">
+      <div className="flex flex-col items-start gap-10 mt-8 mx-auto p-8 md:max-w-1/2 md:mt-30 mb-20">
         <Hero />
         <ProjectShowcase />
         <ProjectLog />
         <TechBlogs />
       </div>
+      <Footer />
     </main>
     
       
---
+++ src/sections/Footer.jsx
@@ -0,0 +1,27 @@
+import {HeartHandshake} from "lucide-react"
+import Button from "../components/Button";
+
+const Footer = () => {
+    return ( 
+        <div className="bg-fixed relative bottom-0 bg-foreground pt-8  flex flex-col justify-center items-center gap-8">
+            <div className="w-1/2 text-center flex flex-col gap-4 items-center py-4">
+            <HeartHandshake color="var(--color-muted)" size={30}/> 
+                <h1 className="font-primary text-background text-xl">About Me</h1>
+                <span className="font-secondary text-sm font-thin text-background">I love learning mathematics in my free time.<br /> I am also a trained Kathak Dancer and a Toastmaster. My strength is my love for my work. Whether I am doing a job or my own business or personal projects, I do it with same joy and that's the joy that brought me back to tech.</span>
+            </div>
+            <h1 className="text-md text-accent-blue-text font-semibold font-primary">OPEN TO WORK</h1>
+
+            <div className="flex gap-4 font-secondary text-background">
+        
+        <Button btnText="Download Resume"/>
+        <Button btnText="Get In Touch"/>
+            </div>
+
+            <div className="w-full text-center py-4 bg-foreground border-t border-muted font-secondary text-muted text-sm">
+                Copyright@Akanksha Deshwal. All rights reserved.
+            </div>
+        </div>
+     );
+}
+ 
+export default Footer;
