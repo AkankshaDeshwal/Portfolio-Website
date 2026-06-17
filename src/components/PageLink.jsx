10a0fff732b5ea5d6a4b8b688075a819de9d9af8
AkankshaDeshwal <adeshwal85@gmail.com>
2026-06-17 23:03:34 +0530

    style:Fix blog heading and link for dark mode

--- src/components/PageLink.jsx
+++ src/components/PageLink.jsx
@@ -1,7 +1,7 @@
 const PageLink = ({name, link}) => {
     return (
         <>
-        <a className="no-underline hover:underline underline-offset-4" href={link} target="_blank">{name}</a>
+        <a className="no-underline hover:underline underline-offset-4 " href={link} target="_blank">{name}</a>
         </>
      );
 }
--- src/components/TechBlogCard.jsx
+++ src/components/TechBlogCard.jsx
@@ -3,9 +3,9 @@ import PageLink from "./PageLink";
 
 const TechBlogCard = () => {
     return ( <div className="font-primary rounded-md p-4 bg-white flex flex-col gap-2 text-foreground">
-        <div className="text-sm font-semibold ">01/ This Blog</div>
+        <div className="text-sm font-semibold text-gray-950">01/ This Blog</div>
         <div className="text-xs text-muted font-secondary">React Fiber is an ongoing reimplementation of React's core algorithm. It is the culmination of over two years of research...</div>
-        <div className=" text-sm  flex items-center"><PageLink name='Read Article'/><ArrowUpRight size={18} /></div>
+        <div className=" text-sm  flex items-center text-gray-950"><PageLink name='Read Article'/><ArrowUpRight size={18} /></div>
 
 
     </div> );
